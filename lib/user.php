ebf60bb93029db32cd0b4aa469d4b7401950e41c
Ganesh Kandu <kanduganesh@gmail.com>
2016-11-12 21:51:32 +0530

    :art: Glyphicon
    :art: Glyphicon

--- lib/user.php
+++ lib/user.php
@@ -13,38 +13,16 @@ class user{
 	var $userdetail = array();
 	
 	function isValidUser($user,$pass){
-		global $userdetail;
-		
-		/* DO NOT REMOVE
-		$conn = new mysqli(DBHOST,DBUSER,DBPASS,DATABASE);
-		// check connection
-		if (mysqli_connect_errno()) {
-		  echo mysqli_connect_error();
-		}
-		$query = "SELECT id, password FROM `".DBPREFIX."user` WHERE username = '".$user."' limit 1;";
-		$result = $conn->query($query);
-		while ($row = $result->fetch_object()) {
-			if(($row->password) == md5($pass)){
-				$userdetail = array('id'=>$row->id,'user'=>$user,'$pass'=>$pass);
-				return true;
-			}else{
-				return false;
-			}
-		}
-		return false;
-		*/
-		
 		$admin = unserialize(ADMIN);
 		if(isset($admin[$user])){
 			if(md5($pass) == $admin[$user]){
-				$userdetail = array('id'=>'1','user'=>$user,'$pass'=>$pass);
+				$this->userdetail = array('id'=>'1','user'=>$user,'$pass'=>$pass);
 				return true;
 			}
 		}
 		return false;
 	}
 	function getUserDetail(){
-		global $userdetail;
-		return $userdetail;
+		return $this->userdetail;
 	}
 }
